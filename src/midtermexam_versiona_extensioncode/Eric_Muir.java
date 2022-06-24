29ac3c9a4612e3ecd57f210c5a2caa15a025ed9d
eric muir <muire@sheridancollege.ca>
2022-06-24 14:08:30 -0400

    Created branch Eric_Muir and java Eric_Muir and added comments to class

---
+++ src/midtermexam_versiona_extensioncode/Eric_Muir.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author TheMu
+ */
+public class Eric_Muir {
+    // Hello, these are my comments for MidTermPartC
+    // Comments added 2:06 PM 6/24/2022
+    // Comment line end
+}
